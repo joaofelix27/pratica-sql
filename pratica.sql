83a3e5ef3594de30a37cba5cb7475491d006f8e1
joaofelix27 <joaofelix27@gmail.com>
2022-07-26 20:54:51 -0300

    Bônus após prazo de entrega

--- pratica.sql
+++ pratica.sql
@@ -9,3 +9,6 @@ SELECT users.id as id,users.name as name,courses.name as course,schools.name as 
 
 __Exericío 04:
 SELECT users.id as id, users.name as name,roles.name as role, companies.name as company,experiences."startDate" as "startDate" FROM experiences JOIN users ON users.id=experiences."userId" JOIN roles ON roles.id=experiences."roleId" JOIN companies ON companies.id=experiences."companyId" WHERE users.id=50 AND experiences."endDate" IS NULL;
+
+__BÔNUS:
+SELECT schools.id as id,schools.name as school, courses.name as course,companies.name as company, roles.name as role FROM applicants JOIN educations ON educations."userId"=applicants."userId" JOIN schools ON schools.id=educations."schoolId" JOIN courses ON courses.id=educations."courseId" JOIN jobs ON jobs.id=applicants."jobId" JOIN companies ON companies.id=jobs."companyId" JOIN roles ON roles.id=jobs."roleId" WHERE roles.name='Software Engineer' AND companies.id=10 AND jobs.active='true';
